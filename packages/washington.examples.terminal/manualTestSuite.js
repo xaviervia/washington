f8568f328e2057e9acf246df8e3ec93173f02cb5
Xavier Via <fernando.via@gmail.com>
2017-03-08 01:26:14 +0100

    Deprecated JSON formatter and cleanup terminal examples

--- packages/washington.examples.terminal/manualTestSuite.js
+++ packages/washington.examples.terminal/manualTestSuite.js
@@ -6,31 +6,31 @@ const multiplication = x => y => x * y
 
 const testSuite = [
   {
-    description: '1 + 1 will be 2',
-    test: () => add(1)(1),
-    expectedValue: 2
+    it: '1 + 1 will be 2',
+    when: () => add(1)(1),
+    shouldEqual: 2
   },
 
   {
-    description: '3 + 2 will be 5',
-    test: () => add(3)(2),
-    expectedValue: 5
+    it: '3 + 2 will be 5',
+    when: () => add(3)(2),
+    shouldEqual: 5
   },
 
   {
-    description: '4 * 5 will be 20',
-    test: () => multiply(4)(5),
-    expectedValue: 20
+    it: '4 * 5 will be 20',
+    when: () => multiply(4)(5),
+    shouldEqual: 20
   },
 
   {
-    description: '4 * 5 will fail to be 25',
-    test: () => multiplication(4)(5),
-    expectedValue: 25
+    it: '4 * 5 will fail to be 25',
+    when: () => multiplication(4)(5),
+    shouldEqual: 25
   },
 
   {
-    description: 'the test is not defined yet'
+    it: 'the test is not defined yet'
   }
 ]
 
--- packages/washington.examples.terminal/withTAPFormatter.js
+++ packages/washington.examples.terminal/withTAPFormatter.js
@@ -1,21 +1,40 @@
 const washington = require('washington')
 const washingtonFormatterTAP = require('washington.formatter.tap')
-const {example, suite} = washington
 
 const add = x => y => x + y
 
 const multiplication = x => y => x * y
 
-const suiteTask = washington(suite(
-  example('1 + 1 will be 2', () => add(1)(1), 2),
+const suiteTask = washington([
+  {
+    it: 'returns 2 when adding 1 and 1',
+    when: () => add(1)(1),
+    shouldEqual: 2
+  },
 
-  example('3 + 2 will be 5', () => add(3)(2), 5),
+  {
+    it: 'returns 3 + 2 will be 5',
+    when: () => add(3)(2),
+    shouldEqual: 5
+  },
 
-  example('4 * 5 will be 20', () => multiply(4)(5), 20),
+  {
+    it: 'returns 20 when multiplying 4 by 5',
+    when: () => multiply(4)(5),
+    shouldEqual: 20
+  },
 
-  example('4 * 5 will fail to be 25', () => multiplication(4)(5), 25),
+  {
+    it: 'does not return 25 when multiplying 4 by 5',
+    when: () => multiplication(4)(5),
+    shouldEqual: 25
+  },
 
-  example('the test is not defined yet')
-), {safe: true})
+  {
+    it: 'is not defined yet'
+  }
+], {safe: true})
 
-washingtonFormatterTAP(suiteTask).run()
+suiteTask
+  .chain(washingtonFormatterTAP(console.log))
+  .run()
