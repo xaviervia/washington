81d210f1de446a542c2395af24f0a74155f04b63
Xavier Via <fernando.via@gmail.com>
2017-03-09 23:47:58 +0100

    Update and fix browser formatter

--- packages/washington.examples.browser/src/index.js
+++ packages/washington.examples.browser/src/index.js
@@ -1,17 +1,37 @@
-import washington, {example, suite} from 'washington'
+import washington from 'washington'
 import washingtonFormatterBrowser from 'washington.formatter.browser'
 
 const suiteTask = washington(
-  suite(
-    example('1 + 2 is 3', check => check(1 + 2), 3),
-    example('2 + 2 is 4', check => check(2 + 2), 4),
-    example('2 + notDefined.number gives runtime error', check => check(2 + ({}).notDefined.number), 4),
-    example('2 + 2 will not be 6', check => check(2 + 2), 6),
-    example('2 + 2 will not get milk for me')
-  ),
+  [
+    {
+      description: '1 + 2 is 3',
+      test: check => check(1 + 2),
+      shouldEqual: 3
+    },
+    {
+      description: '2 + 2 is 4',
+      test: check => check(2 + 2),
+      shouldEqual: 4
+    },
+    {
+      description: '2 + notDefined.number gives runtime error',
+      test: check => check(2 + ({}).notDefined.number),
+      shouldEqual: 4
+    },
+    {
+      description: '2 + 2 will not be 6',
+      test: check => check(2 + 2),
+      shouldEqual: 6
+    },
+    {
+      description: '2 + 2 will not get milk for me'
+    }
+  ],
   {
     safe: true
   }
 )
 
-washingtonFormatterBrowser(suiteTask).run()
+suiteTask
+  .chain(washingtonFormatterBrowser(console.log))
+  .run()
